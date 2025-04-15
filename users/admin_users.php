d31a674bc15737cb75535cdc32204a79a5ca86b0
Desmond <desmondhjs@gmail.com>
2025-04-15 15:36:04 +0800

    make navigation dropdown clickable

--- users/admin_users.php
+++ users/admin_users.php
@@ -20,6 +20,7 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Users - NoiceFoodie</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body class="bg-light">
     <?php include '../includes/navigation.php'; ?>
